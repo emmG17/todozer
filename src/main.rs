0af396be80b6ad491980c5bda873a2d58d2e62c0
Emmanuel Gonzalez <emm.gs17@gmail.com>
2025-04-25 19:16:27 -0600

    feat: create the CLI interface

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use clap::Parser;
+
+/// Finds TODOs in code and outputs them as JSON
+#[derive(Parser)]
+#[command(name = "todozer")]
+#[command(version = "0.1")]
+#[command(about = "Scan source files and extract TODOs", long_about = None)]
+pub struct Cli {
+    /// The path to the source code to scan
+    #[arg(short, long, default_value = ".")]
+    pub path: String,
+}
+
+fn main() {
+    let args = Cli::parse();
+    println!("Path: {}", args.path);
+}
